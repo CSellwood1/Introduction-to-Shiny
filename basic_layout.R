3541d30783e1e41c226020ebd6ccbfc2fdb4fd92
Colleen Sellwood <C.Sellwood@newcastle.ac.uk>
2021-04-29 14:49:44 +0100

    Added a title panel

--- basic_layout.R
+++ basic_layout.R
@@ -2,10 +2,20 @@
 library(shiny)
 
 # Define UI ----
-ui <- fluidPage()
+ui <- fluidPage(
+  #add a title panel
+  titlePanel("title panel"),
+  
+  sidebarLayout(
+    sidebarPanel("sidebar panel"),
+    mainPanel("main panel")
+  )
+  
+)
 
 # Define server logic ----
 server <- function(input, output) {}
 
 # Run the app ----
 shinyApp(ui = ui, server = server)
+
