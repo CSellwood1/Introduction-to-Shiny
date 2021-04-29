62b789068d25ce4a16fad7bf2b365caaa639f54a
Colleen Sellwood <C.Sellwood@newcastle.ac.uk>
2021-04-29 15:02:08 +0100

    Added a paragraph in the main text

--- basic_layout.R
+++ basic_layout.R
@@ -11,8 +11,9 @@ ui <- fluidPage(
     mainPanel("main panel"), position = c("right")),
   mainPanel(
     h1("This is the main heading for my app"),
-    h2("here is a subheading"))
-  
+    h2("here is a subheading"),
+    p("This website will be to help planners assess potential windfarm development areas in Cumbria, and achieve a strong balance between different interest groups and other users."))
+    
 )
 
 # Define server logic ----
