6ec8c563427977fbf44825e5caa7861207e3e0e9
Colleen Sellwood <C.Sellwood@newcastle.ac.uk>
2021-04-29 14:42:16 +0100

    Set up the ui and the server. Ran the app

--- basic_layout.R
+++ basic_layout.R
@@ -1,1 +1,11 @@
 #how to lay out a user-interface
+library(shiny)
+
+# Define UI ----
+ui <- fluidPage()
+
+# Define server logic ----
+server <- function(input, output) {}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
